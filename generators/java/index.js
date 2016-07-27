3c7bcbd498f8081f7f979ab63000588416136377
urosjarc <urosjarc@users.noreply.github.com>
2016-07-27 16:10:35 +0200

    Creating java subgenerator logics.

--- generators/java/index.js
+++ generators/java/index.js
@@ -1,10 +1,38 @@
 'use strict';
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
-var yosay = require('yosay');
+var fs = require('fs');
+var Q = require('./prompt.json');
 
 module.exports = yeoman.Base.extend({
-  prompting: function () {
-    this.log('Selecting java-terminal template...');
-  }
+	prompting: function () {
+		var self = this;
+
+		var forAllTemps = fs.readdirSync(this.templatePath('_forAll'));
+		var dirList = fs.readdirSync(this.sourceRoot());
+
+		if (!this.config.get('projectType')) {
+
+			for(var i in dirList){
+				if(dirList[i] != "_forAll")
+					Q.projectType[0].choices.push(dirList[i]);
+			}
+
+			return this.prompt(Q.projectType).then(function (A0) {
+				self.props = A0;
+
+				Q.templates[0].choices = fs.readdirSync(this.templatePath(A0.projectType));
+				for(var i in forAllTemps)
+					Q.templates[0].choices.push(forAllTemps[i]);
+
+				return self.prompt(Q.templates).then(function(A1){
+					self.props.templates = A1.templates;
+				});
+
+			}.bind(this));
+		}
+	},
+	configuring: function () {
+		this.log(this.props);
+	}
 });
