92cf9c6d29ff4d7ec286bd188572a19afb5108a4
urosjarc <urosjarc@users.noreply.github.com>
2016-07-27 13:29:39 +0200

    Adding composing for subgenerators.

--- generators/app/index.js
+++ generators/app/index.js
@@ -8,31 +8,12 @@ module.exports = yeoman.Base.extend({
   prompting: function () {
     var self = this;
 
-    this.log(yosay(Q.yeosay));
+    this.log(yosay("Welcome to the delightful generator-generate generator!"));
 
     return this.prompt(Q.lang).then(function (A0) {
-      var _self = this;
-      _self.props = A0;
-
-      return self.prompt(Q[A0.lang]).then(function (A1) {
-        return _self.props.temp = A1.temp;
-      }.bind(_self));
-
+      self.props = A0;
+      self.composeWith('generate:' + A0.lang);
     }.bind(this));
-
-
-  },
-
-  writing: function () {
-
-    this.log(this.props);
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  },
-
-  install: function () {
-    this.installDependencies();
   }
+
 });
