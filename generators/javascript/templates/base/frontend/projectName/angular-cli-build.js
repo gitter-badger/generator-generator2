e01ea76aaa44524ca78b33b6d16805a03c088928
urosjarc <urosjarc@users.noreply.github.com>
2016-08-01 14:50:12 +0200

    Adding bootstrap to the application

--- generators/javascript/templates/base/frontend/projectName/angular-cli-build.js
+++ generators/javascript/templates/base/frontend/projectName/angular-cli-build.js
@@ -15,7 +15,10 @@ module.exports = function(defaults) {
       'es6-shim/es6-shim.js',
       'reflect-metadata/**/*.+(ts|js|js.map)',
       'rxjs/**/*.+(js|js.map)',
-      '@angular/**/*.+(js|js.map)'
+      '@angular/**/*.+(js|js.map)',
+      'bootstrap/dist/js/bootstrap.min.js',
+      'bootstrap/dist/css/bootstrap.min.css',
+      'jquery/dist/jquery.min.js'
     ]
   });
 };
