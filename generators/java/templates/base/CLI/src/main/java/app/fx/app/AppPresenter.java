ea9cc104298a7f62bdc4680c04e4b5b9f6e83f22
urosjarc <urosjarc@users.noreply.github.com>
2016-08-05 15:33:44 +0200

    Finishing javafx structure

--- generators/java/templates/base/CLI/src/main/java/app/fx/app/AppPresenter.java
+++ generators/java/templates/base/CLI/src/main/java/app/fx/app/AppPresenter.java
@@ -3,30 +3,15 @@ package app.fx.app;
 import app.fx.app.dialogs.about.About;
 import app.fx.app.dialogs.help.Help;
 import javafx.application.Platform;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Tab;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class AppPresenter implements Initializable {
 
-	@FXML
-	private Tab entries;
-	@FXML
-	private Tab realizations;
-	@FXML
-	private Tab bookmarks;
-
 	@Override
-	public void initialize(URL url, ResourceBundle rb) {
-
-//		entries.setContent(EntriesCtrl.getView());
-//		realizations.setContent(RealizationsCtrl.getView());
-//		bookmarks.setContent(BookmarksCtrl.getView());
-
-	}
+	public void initialize(URL url, ResourceBundle rb) { }
 
 	public void exit(){
 		Platform.exit();
